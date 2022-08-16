c119b52ae4f95f33bc9fb2e4bd9d888ff3d4ba11
Murilo <murilopaiva22@gmail.com>
2022-08-16 17:59:31 -0300

    #03 - Estilizando componentes

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import ListaDeNotas from './components/ListaDeNotas';
-import FormularioCadastro from './components/FormularioCadastro';
-class App extends Component{
-  render(){
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+  render() {
     return (
-      <section>
-      <FormularioCadastro/>
-      <ListaDeNotas/>
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
       </section>
-      
     );
   }
 }
